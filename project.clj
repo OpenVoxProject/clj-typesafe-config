86af195608afda520695d2fcbcb8e04fe8b45841
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-30 19:06:21 -0800

    Remove old lein-release plugin declaration

--- project.clj
+++ project.clj
@@ -11,8 +11,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.typesafe/config "1.4.1"]]
 
-  :plugins [[lein-release "1.0.5"]]
-
   :lein-release        {:scm          :git
                         :deploy-via   :lein-deploy}
 
